a318d78cbe5d2b6e77fe8de213d31829503f7e43
Nayan <dreamerboynayan@gmail.com>
2025-08-30 06:18:07 +0600

    Linear Search on unsorted array

--- Arrays Basics.js
+++ Arrays Basics.js
@@ -4,29 +4,28 @@
 
 // Function to implement search operation
 
-function findValue(arr,n,target){
+function findTargetValue(arr, n , target){
     let i;
-    for(let i = 0; i < n; i ++){
+    for(let i = 0; i < n ; i ++){
         if(arr[i] == target){
             return i;
         }
     }
-
-    return -1
+    return -1;
 }
 
  // Driver program
 
-let arr = [12,25,13,11,16,17,10,14,15];
+let arr = [23,45,65,32,25,30,49,56];
 let n = arr.length;
 
 // Using a last element as search element
 
-let target = 27;
-let position = findValue(arr,n,target);
+let target = 49;
+let position = findTargetValue(arr,n,target);
 
 if(position == -1){
-    console.log('Value not founded in this array!')
+    console.log(`Value not founded in this array !`);
 }else{
     console.log(`Value founded in this index ${position}`)
 }
