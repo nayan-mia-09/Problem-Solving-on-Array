82121534d903e5c315d5229b79085f283e6e1465
Nayan <dreamerboynayan@gmail.com>
2025-08-30 07:05:44 +0600

    Linear search in sorted array

--- Arrays Basics.js
+++ Arrays Basics.js
@@ -29,3 +29,32 @@ if(position == -1){
 }else{
     console.log(`Value founded in this index ${position}`)
 }
+
+// search in sorted array
+
+function sortedArrayLinearSearch(arr2, n2 , target2){
+    let i ;
+    for(let i = 0; i < n2; i ++){
+        if(arr2[i] == target2){
+            return i;
+        }
+    }
+    return -1;
+}
+
+ // Driver program
+let arr2 = [10,12,14,16,18,20,22,24,26,28,30];
+
+let n2 = arr2.length;
+
+// Using a last element as search element
+
+let target2 = 22;
+let position2 = sortedArrayLinearSearch(arr2, n2 , target2);
+
+if(position2 == -1){
+    console.log(`Element  not founded on this array !`);
+}else{
+    console.log(`Element founded in this index ${position2}`);
+}
+
